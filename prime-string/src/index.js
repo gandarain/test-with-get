1f4b9dbdb3703e42d3e2aaaed592361365c309b0
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-03-27 21:25:36 +0700

    prime string

--- prime-string/src/index.js
+++ prime-string/src/index.js
@@ -1,14 +1,41 @@
 const readline = require('readline');
 
-/**
- * 
- * @param {string} str - str
- * @returns {number} split prime number
- */
-const countPrimeStrings = (str) => {
-  const splitPrime = 0;
-  
-  return splitPrime;
+let MOD = 1000000007;
+
+function isPrime(number){
+	let num = parseInt(number);
+	
+	for(let i = 2; i * i <= num; i++)
+	{
+	if ((num % i) == 0)
+		return false;
+	}
+	return num > 1 ? true : false;
+}
+
+function countPrimeStrings(number,i){
+	if (i == 0)
+		return 1;
+		
+	let cnt = 0;
+
+	for(let j = 1; j <= 6; j++){
+    if (i - j >= 0 &&
+      number[i - j] != '0' &&
+      isPrime(number.substring(i - j, i)))
+    {
+      cnt += countPrimeStrings(number,
+                    i - j);
+      cnt %= MOD;
+    }
+	}
+	
+	return cnt;
+}
+
+function countingPrime(str) {
+	let l = str.length;
+	return countPrimeStrings(str, l);
 }
 
 function main() {
@@ -18,7 +45,7 @@ function main() {
   });
   
   rl.question('Enter the string of number: ', (string) => {
-    console.log(`Split prime result: ${countPrimeStrings(string)}`);
+    console.log(`Split prime result: ${countingPrime(string)}`);
     rl.close();
   });
 };
